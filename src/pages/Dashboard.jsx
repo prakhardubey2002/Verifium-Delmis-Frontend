6afdd4b59bcf0cb20820b751d154da49560905c7
Prakhar Dubey <prakhardubey2002@gmail.com>
2023-09-16 14:57:20 +0530

    changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -21,12 +21,12 @@ ChartJS.register(
   Legend
 );
 export const data = {
-  labels: ['Monthly Activity', 'Active POAPS', 'Minted Credential', 'Notification', 'Thing 5', 'Thing 6'],
+  labels: ['Monthly Activity', 'Active POAPS', 'Minted Credential', 'Notification', 'Todo Items', 'Requests'],
   datasets: [
     {
-      label: 'User Details',
-      data: [2, 9, 3, 5, 2, 3],
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      label: ' ',
+      data: [7, 9, 5, 1, 8, 6],
+      backgroundColor: 'rgb(250,226,147)',
       borderColor: 'rgb(217,242,126)',
       borderWidth: 1,
     },
@@ -43,16 +43,20 @@ const Dashboard = () => {
               <span> Hi,</span>Prakhar!
               What do you want
               to <span>Learn </span> Today?
+              <p>
+            Invest yourself and take the first step towards achiving your dream
+          </p>
             </div>
           <Lottie className='useranimation' animationData={Ani} loop={true} />
           </h2>
-          <p>
-            invest yourself and take the first step towards achiving your dream
-          </p>
+          
         </div>
         <div className="radarchart">
           <Radar data={data} />
         </div>
+      </div>
+      <div className="bottom">
+
       </div>
     </div>
   )
